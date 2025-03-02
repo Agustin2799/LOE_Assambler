e6812dcdff9f7bae325e745fd4924872618c621c
Agustin2799 <140901456+Agustin2799@users.noreply.github.com>
2025-03-02 02:24:23 -0300

    t

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + Reactt</h1>
       <div className="card">
         <button onClick={()=> actions.increaseCounter()}>
           count is {store.counter}
